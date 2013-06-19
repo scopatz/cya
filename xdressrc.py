b2d4d4468da2a5836a1e0fa495667ddcadc411d7
Anthony Scopatz <scopatz@gmail.com>
2013-06-19 00:14:33 -0500

    more update

--- xdressrc.py
+++ xdressrc.py
@@ -1,9 +1,19 @@
-sourcedir = "/usr/include"
+import os
+
+sourcedir = "/home/scopatz/Downloads/hdf5-1.8.11/src"
 package = "cya"
 
-classes = [('*', 'hdf5')]
-functions = [('*', 'hdf5')]
-variables = [('*', 'hdf5')]
-
+# Don't do too much
 make_extra_types = False
 make_stlcontainers = False
+defines = ["H5_NO_DEPRECATED_SYMBOLS", "DIR=__dirstream"]
+
+# Find source files and search for their contents
+_fs = set(os.listdir(sourcedir))
+_fs -= set(['H5FDsec2.c'])
+_fs = sorted(_fs)
+_fs = [('*', f.rsplit('.', 1)[0], 'hdf5') for f in _fs if f.endswith('.c')]
+classes = list(_fs)
+functions = list(_fs)
+variables = list(_fs)
+
