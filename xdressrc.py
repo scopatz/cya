0b04655392381e1327244d45331362b6592835fb
Anthony Scopatz <scopatz@gmail.com>
2013-06-19 02:05:28 -0500

    almost

--- xdressrc.py
+++ xdressrc.py
@@ -12,7 +12,8 @@ clear_parser_memo_period = 25
 
 # Find source files and search for their contents
 _fs = set(os.listdir(sourcedir))
-_fs -= set(['H5FDsec2.c', 'H5PL.c', 'H5FDstdio.c', 'H5FDcore.c', 'H5Dbtree.c'])
+_fs -= set(['H5FDsec2.c', 'H5PL.c', 'H5FDstdio.c', 'H5FDcore.c', 'H5Dbtree.c', 
+            'H5trace.c', 'H5FDlog.c', 'H5system.c'])
 _fs = sorted(_fs)
 _fs = [('*', f.rsplit('.', 1)[0], 'hdf5') for f in _fs if f.endswith('.c')]
 #_fs = [('*', 'H5PL', 'hdf5')]
