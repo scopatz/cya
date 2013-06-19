d0e93cb8e0fb13dd35e931d517e63578fe386943
Anthony Scopatz <scopatz@gmail.com>
2013-06-19 01:34:43 -0500

    more updates

--- xdressrc.py
+++ xdressrc.py
@@ -8,11 +8,15 @@ make_extra_types = False
 make_stlcontainers = False
 defines = ["H5_NO_DEPRECATED_SYMBOLS", "DIR=__dirstream"]
 
+clear_parser_memo_period = 25
+
 # Find source files and search for their contents
 _fs = set(os.listdir(sourcedir))
-_fs -= set(['H5FDsec2.c'])
+_fs -= set(['H5FDsec2.c', 'H5PL.c', 'H5FDstdio.c', 'H5FDcore.c', 'H5Dbtree.c'])
 _fs = sorted(_fs)
 _fs = [('*', f.rsplit('.', 1)[0], 'hdf5') for f in _fs if f.endswith('.c')]
+#_fs = [('*', 'H5PL', 'hdf5')]
+
 classes = list(_fs)
 functions = list(_fs)
 variables = list(_fs)
