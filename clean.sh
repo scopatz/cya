1e1ce14c1e0b35f47c819c3619b84a1ab8ddaed1
Anthony Scopatz <scopatz@gmail.com>
2013-06-19 22:20:12 -0500

    more updates

--- clean.sh
+++ clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-rm -r build cya /home/scopatz/Downloads/hdf5-1.8.11/src/*pkg.c
+rm -r build cya /home/scopatz/Downloads/hdf5-1.8.11/src/*pkg.c \
+                /home/scopatz/Downloads/hdf5-1.8.11/src/*public.c \
+                /home/scopatz/Downloads/hdf5-1.8.11/src/*private.c 
--- xdressrc.py
+++ xdressrc.py
@@ -6,7 +6,7 @@ sourcedir = "/home/scopatz/Downloads/hdf5-1.8.11/src"
 package = "cya"
 
 # Don't do too much
-make_extra_types = False
+make_extra_types = True
 make_stlcontainers = False
 defines = ["H5_NO_DEPRECATED_SYMBOLS", 
            "DIR=__dirstream", ]
@@ -29,6 +29,38 @@ for _pf in _pkgfiles:
         _f.write(_code)
     defines.append(_base + "_PACKAGE")
 
+_pkgfiles = glob(os.path.join(sourcedir, '*public.h'))
+for _pf in _pkgfiles:
+    _src = _pf.rsplit('.', 1)[0] + '.c'
+    _base = os.path.split(_pf)[1][:-8]
+    _code = '#include "H5public.h"\n'
+    if _base in set(['H5R', 'H5FS', 'H5B2', 'H5MF', 'H5F', 'H5HF', 'H5HG']):
+        _code += '#include <stdio.h>\n'
+    #if _base == 'H5SM':
+    #    _code += '#include <stdlib.h>\n#include "H5FLprivate.h"\n'
+    if _base == 'H5Z':
+        _code += '#include "H5Iprivate.h"\n'
+    _code += '#include "{base}public.h"\n'.format(base=_base)
+    with io.open(_src, 'wb') as _f:
+        _f.write(_code)
+
+_pkgfiles = glob(os.path.join(sourcedir, '*private.h'))
+for _pf in _pkgfiles:
+    _src = _pf.rsplit('.', 1)[0] + '.c'
+    _base = os.path.split(_pf)[1][:-9]
+    _code = '#include "H5public.h"\n'
+    if _base in set(['H5R', 'H5FS', 'H5B2', 'H5MF', 'H5F', 'H5HF', 'H5HG']):
+        _code += '#include <stdio.h>\n'
+    #if _base == 'H5SM':
+    #    _code += '#include <stdlib.h>\n#include "H5FLprivate.h"\n'
+    if _base == 'H5Z':
+        _code += '#include "H5Iprivate.h"\n'
+    _code += '#include "{base}private.h"\n'.format(base=_base)
+    with io.open(_src, 'wb') as _f:
+        _f.write(_code)
+
+
+
 # Find source files and search for their contents
 _fs = set(os.listdir(sourcedir))
 _fs -= set(['H5FDsec2.c', 'H5PL.c', 'H5FDstdio.c', 'H5FDcore.c', 'H5Dbtree.c', 
@@ -36,9 +68,7 @@ _fs -= set(['H5FDsec2.c', 'H5PL.c', 'H5FDstdio.c', 'H5FDcore.c', 'H5Dbtree.c',
 _fs = sorted(_fs)
 _fs = [('*', f.rsplit('.', 1)[0], 'hdf5') for f in _fs \
        if f.endswith('.c')]
-#_fs += [('*', 'H5Ppkg', 'hdf5')]
 
 classes = list(_fs)
 functions = list(_fs)
 variables = list(_fs)
-
